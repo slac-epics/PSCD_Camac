c84e1959219611a35ccb19c93c140155e9ea6562
Kristi L. Luchini <luchini@slac.stanford.edu>
2010-12-01 05:39:41 +0000

    break out of for loop after match found for PP1 rule

--- PDUApp/src/PDUII_360Task.c
+++ PDUApp/src/PDUII_360Task.c
@@ -1,5 +1,5 @@
 /***************************************************************************\
- **   $Id: PDUII_360Task.c,v 1.11 2010/11/09 21:41:11 rcs Exp $
+ **   $Id: PDUII_360Task.c,v 1.12 2010/12/01 00:53:16 luchini Exp $
  **   File:              PDUII_360Task.c
  **   Author:            Sheng Peng
  **   Email:             pengsh2003@yahoo.com
@@ -284,7 +284,7 @@ static int PDUIIFidu360Task(void * parg)
                                 {
                                     pttDelayNew =  pPDUIIModule->rules[loopch][looprule].pttDelay;
                                     if(PDUII_360T_DEBUG >= 1) errlogPrintf("Matched rule[%d] module[C%d,N%d],Channel[%d]\n", looprule, pPDUIIModule->c, pPDUIIModule->n,loopch);
-                                    continue; /* stop matching */
+                                    break; /* stop matching */
                                 }
                             }
                         }
