866cd6da071a0d40b2282161e9932b69b5c06fcb
Kristi L. Luchini <luchini@slac.stanford.edu>
2011-09-16 03:20:37 +0000

    Add Crate Verifier Application

---
+++ CrateVerifierApp/src/CVSub_proto.h
@@ -0,0 +1,31 @@
+/*
+=============================================================
+
+  Abs:  Crate Verifier Subroutine Records prototypes
+
+  Name: CVSub_proto.h
+   
+  Side: genSubRecord.h
+
+  Auth: 02-Jul-2010, K. Luchini       (LUCHINI)
+  Rev : dd-mmm-yyyy, Reviewer's Name  (USERNAME)
+
+-------------------------------------------------------------
+  Mod:
+        dd-mmm-yyyy, First Lastname   (USERNAME):
+          comment
+
+=============================================================
+*/
+
+#ifndef _CVSUB_PROTO_H_
+#define _CVSUB_PROTO_H_
+
+
+vmsstat_t CV_Init( genSubRecord *sub_ps);
+vmsstat_t CV_STAT( genSubRecord *sub_ps);
+
+#endif /*_CVSUB_PROTO_H_ */
+
+
+
